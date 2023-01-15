a725f34a7ffe2bc7a5cda41247585da4b2a54ea3
meteor <tjdans34568@gmail.com>
2023-01-15 15:26:51 +0900

    button test 완료 / 페이지 라우팅 세팅 시작

--- lib/component/text_component.dart
+++ lib/component/text_component.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TextComponent extends StatelessWidget {
-  const TextComponent({Key? key}) : super(key: key);
+  final String textContent;
+  final double textFontSize;
+  final Color textColor;
+  final FontWeight textFontWeight;
+
+  const TextComponent(
+      {required this.textContent,
+      required this.textFontSize,
+      required this.textColor,
+      required this.textFontWeight,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      textContent,
+      style: TextStyle(
+          fontSize: textFontSize, color: textColor, fontWeight: textFontWeight),
+    );
   }
 }
--- lib/screen/button_tr/button_screen.dart
+++ lib/screen/button_tr/button_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meteor_flutter_training/component/text_component.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({Key? key}) : super(key: key);
@@ -21,7 +22,9 @@ class ButtonScreen extends StatelessWidget {
                 onPressed: () {
                   print("Elevated Button Style");
                 },
+                // ButtonStyle 은 상태에 따라서 제어하는 용도인가 보군 MaterialStateProperty 필수인듯
                 style: ButtonStyle(
+                    shadowColor: MaterialStateProperty.all(Colors.blueAccent),
                     // Material State
                     // hovered - 호버링 상태 ( 마우스 커서를 올려놓은 상태 )
                     // focused - 포커스 되었을 때 상태 (ex: 텍스트 핋드)
@@ -34,27 +37,68 @@ class ButtonScreen extends StatelessWidget {
                     backgroundColor:
                         // MaterialStateProperty.all(Colors.pinkAccent),
                         MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Colors.blueAccent;
-                  }
-                  ;
-                  return Colors.pinkAccent;
-                }), foregroundColor:
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.blueAccent;
+                      }
+                      ;
+                      return Colors.pinkAccent;
+                    }),
+                    foregroundColor:
                         MaterialStateProperty.resolveWith((states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Colors.green;
-                  }
-                  return Colors.blueAccent;
-                })),
-                child: const Text(
-                  "Elevated Button Style",
-                  style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
-                ),
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.green;
+                      }
+                      return Colors.blueAccent;
+                    })),
+                child: const TextComponent(
+                    textContent: "Elevated Button ButtonStyle",
+                    textFontSize: 15.0,
+                    textColor: Colors.white,
+                    textFontWeight: FontWeight.w700),
               ),
-              ElevatedButton(onPressed: () {}, child: Text(""))
+              ElevatedButton(
+                  onPressed: () {
+                    print("Elevated Button StyleForm");
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.blueAccent,
+                      elevation: 10,
+                      foregroundColor: Colors.blueAccent,
+                      backgroundColor: Colors.pinkAccent),
+                  child: const TextComponent(
+                    textContent: "Elevated Button StyleForm",
+                    textFontSize: 15.0,
+                    textColor: Colors.white,
+                    textFontWeight: FontWeight.w700,
+                  )),
+              OutlinedButton(
+                  onPressed: () {
+                    print("OutlinedButton");
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.teal),
+                      shadowColor: MaterialStateProperty.all(Colors.pink),
+                      elevation: MaterialStateProperty.all(10.0)),
+                  child: const TextComponent(
+                    textContent: "OutlinedButton",
+                    textColor: Colors.white,
+                    textFontSize: 15.0,
+                    textFontWeight: FontWeight.w700,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    print("TextButton");
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.greenAccent,
+                  ),
+                  child: TextComponent(
+                    textContent: "TextButton",
+                    textColor: Colors.white,
+                    textFontSize: 15.0,
+                    textFontWeight: FontWeight.w700,
+                  ))
             ],
           ),
         ),
