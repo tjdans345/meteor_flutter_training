17079f2e3570b376fac522d1be67ed6b5a5d86fe
meteor <tjdans34568@gmail.com>
2023-01-15 14:27:50 +0900

    button training ~ing

--- lib/button_tr/button_screen.dart
+++ lib/button_tr/button_screen.dart
@@ -8,13 +8,37 @@ class ButtonScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("버튼"),
+          title: const Text("버튼"),
           backgroundColor: Colors.green,
         ),
-        body: Column(
-          children: [
-            ElevatedButton(onPressed: () {}, child: )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(" 하이하이 ");
+                },
+                child: const Text("Elevated Button"),
+                style: ButtonStyle(
+                    // Material State
+                    // hovered - 호버링 상태 ( 마우스 커서를 올려놓은 상태 )
+                    // focused - 포커스 되었을 때 상태 (ex: 텍스트 핋드)
+                    // pressed - 눌렀을 때
+                    // dragged - 드래그 됐을 때
+                    // selected - 선택 됬을 때
+                    // scrollUnder - 다른 컴포넌트 밑으로 스크롤링 되었을 때
+                    // disabled - 비활성화 됐을 때
+                    // error - 에러 상태
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.pinkAccent),
+                    foregroundColor:
+                        MaterialStateProperty.resolveWith((states) => null)),
+              )
+            ],
+          ),
         ),
       ),
     );
